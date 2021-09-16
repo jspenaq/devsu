b696a4eaabf00ab906b0b5e3fea59d0667b70e99
Juan Sebastian Peña <jspenaq@unal.edu.co>
2021-09-16 20:51:59 +0100

    first commit in github

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
+  console.log("Hello World!");
   res.send("Hello World!");
 });
 
 app.get("/hello/:name", (req, res) => {
+  console.log(`Hello ${req.params.name}!`);
   res.send(`Hello ${req.params.name}!`);
 });
 
